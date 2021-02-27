397776da7ca90a2003e020b45d932ba4f8a43597
lander-creator <61006702+lander-creator@users.noreply.github.com>
2021-02-27 17:38:42 +0100

    Add files via upload

---
+++ 01-ESP32-Assignment 1/cloud_en_security___button.ino
@@ -0,0 +1,183 @@
+// library
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <TaskScheduler.h>
+#include "wifi.h"
+//-------------------------------------------------------------------------------
+//                              WiFi
+const char *ssid = "Netwerk-TVS";   // name of your WiFi network
+const char *password = "876354DF34"; // password of the WiFi network
+WiFiClient wifiClient;
+//-------------------------------------------------------------------------------
+//                         MQTT ID & TOPIC
+#define MQTT_SERIAL_PUBLISH_CH "Lander/measurement"
+const char *TOPIC1 = "Lander/button";  // Topic to subcribe to
+#define MQTT_SERIAL_RECEIVER_CH "Lander/led"
+const String DEVICE_ID = "Lander";
+//-------------------------------------------------------------------------------
+//                            broker IP
+const char* mqtt_server  = "54.229.150.108";
+const int mqtt_port  = 1883;
+PubSubClient client(wifiClient);
+//-------------------------------------------------------------------------------
+                            //DHT11
+#define LED_PIN 2
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+//-------------------------------------------------------------------------------
+
+const int button = 5;
+
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      //Once connected, publish an announcement...
+      client.publish("u0118137/connected", "hello world");
+      // ... and resubscribe
+      client.subscribe(MQTT_SERIAL_RECEIVER_CH);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte *payload, unsigned int length) {
+
+   int value = (int) *payload;
+  
+    Serial.println("-------new message from broker-----");
+    Serial.print("channel:");
+    Serial.println(topic);
+    Serial.print("data:");  
+    Serial.write(payload, length);
+    Serial.println();
+
+
+    //contol LED
+    if(value == 49){                //1 in ASCII
+      digitalWrite(LED_PIN,1);
+      Serial.print("Turning on LED");
+    }
+    else if(value == 48){         //0 in ASCII
+      digitalWrite(LED_PIN,0);
+      Serial.print("Turning off LED");
+    }
+    
+    Serial.println();
+}
+
+
+
+// We declare the function that we are going to use
+void read_dht();
+
+// We create the Scheduler that will be in charge of managing the tasks
+Scheduler runner;
+
+// We create the task indicating that it runs every 500 milliseconds, forever, and call the led_blink function
+Task readDHT(2000, TASK_FOREVER, &read_dht);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);// Set time out for 
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  reconnect();
+
+  pinMode (LED_PIN, OUTPUT);
+  pinMode(button, INPUT);
+
+  dht.begin();
+
+  // We add the task to the task scheduler
+  runner.addTask(readDHT);
+  
+  // We activate the task
+  readDHT.enable();
+  
+}
+
+void publishSerialData(char *serialData){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.publish(MQTT_SERIAL_PUBLISH_CH, serialData);
+}
+
+
+
+
+void loop() {
+   client.loop();
+   if (Serial.available() > 0) {
+     char mun[501];
+     memset(mun,0, 501);
+     Serial.readBytesUntil( '\n',mun,500);
+     publishSerialData(mun);
+   }
+   if (digitalRead(button) == LOW){
+      char* message = "1";
+      int length = strlen(message);
+      boolean retained = true;
+      client.publish(TOPIC1,(byte*)message,length,retained);
+      Serial.println((String)TOPIC1 + " => btn");
+   }
+   else {
+    char* message = "0";
+    delay(2000);
+    int length = strlen(message);
+    boolean retained = true;
+    client.publish(TOPIC1,(byte*)message,length,retained);
+    Serial.println((String)TOPIC1 + " => btn");
+   }
+}
+
+
+void read_dht(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.println(h);
+  Serial.println(t);
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  else{
+    String json = "{\"device_id\":\""+DEVICE_ID+"\",\"temperature\":\""+String(t)+"\",\"humidity\":\""+String(h)+"\"}";
+    char jsondata[json.length()+1];
+    json.toCharArray(jsondata,json.length()+1);
+    publishSerialData(jsondata);
+  }
+}
